1a08516d7dfe546d3df71d5e2edd4a5b7fcc3b41
martapastorperez <martapastorperez97@gmail.com>
2024-10-07 12:35:43 +0200

    feat: POST project

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { ProjectController } from "../controllers/ProjectController";
+import { body } from 'express-validator';
+import { handleInputErrors } from "../middleware/validation";
+
+const router=Router()
+
+router.post('/',
+    body('projectName').notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName').notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description').notEmpty().withMessage('La descripcion del proyecto es obligatorio'),
+    handleInputErrors,
+    ProjectController.createProject)
+router.get('/',ProjectController.getAllProjects)
+
+export default router
