e995c25c1d3c317b38fb5f23997b97ac5283d8ce
martapastorperez <martapastorperez97@gmail.com>
2024-10-07 12:48:37 +0200

    feat: GET all projects

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -4,19 +4,20 @@ import Project from "../models/Project"
 export class ProjectController{
     static createProject=async(req:Request, res:Response)=>{
        const project=new Project(req.body)
-
        try {
         await project.save() 
         res.send('Proyecto creado correctamente')
-
        } catch (error) {
         console.log(error);
-        
        }
-        
     }
 
     static getAllProjects=async(req:Request, res:Response)=>{
-        res.send('Todos los proyectos')
+        try {
+            const projects=await Project.find({})
+            res.json(projects)
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
