25cb411cf37823057f29fff06a6c9131a8deccb8
martapastorperez <martapastorperez97@gmail.com>
2024-10-08 17:22:45 +0200

    feat: model tasks

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,10 +1,12 @@
-import mongoose,{ Schema, Document} from "mongoose";
+import mongoose,{ Schema, Document, PopulatedDoc, Types} from "mongoose";
+import { ITask } from "./Task";
 
 
-export type ProjectType=Document &{
+export interface IProject extends Document {
     projectName:string
     clientName:string
     description:string
+    tasks:PopulatedDoc<ITask & Document>[]
 }
 
 const ProjectSchema: Schema= new Schema({
@@ -22,8 +24,14 @@ const ProjectSchema: Schema= new Schema({
         type:String,
         require:true,
         trim:true,
-    }
-})
+    },
+    tasks:[
+        {
+            type:Types.ObjectId,
+            ref:'Task'
+        }
+    ]
+},{timestamps:true})
 
-const Project=mongoose.model<ProjectType >('Project', ProjectSchema)
+const Project=mongoose.model<IProject >('Project', ProjectSchema)
 export default Project
