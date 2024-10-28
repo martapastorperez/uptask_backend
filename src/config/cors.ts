dbcd9626a32adcc9377f15204be250543ec81fff
martapastorperez <martapastorperez97@gmail.com>
2024-10-28 12:55:57 +0100

    feat: cors postman

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -4,7 +4,9 @@ import { CorsOptions } from "cors";
 export const corsConfig:CorsOptions={
     origin: function(origin, callback){
         const whitelist=[process.env.FRONTEND_URL]
-
+if(!origin){//for bypassing postman req with  no origin
+      return callback(null, true);
+    }
         if(whitelist.includes(origin)){
             callback(null,true)
         }else{
