25c6d5e93fcb9fab212e3851ea4f3a01cd324c1e
martapastorperez <martapastorperez97@gmail.com>
2024-11-13 18:02:46 +0100

    feat: add manager

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -4,6 +4,8 @@ import Project from "../models/Project"
 export class ProjectController{
     static createProject=async(req:Request, res:Response)=>{
        const project=new Project(req.body)
+       //asignar manager
+       project.manager=req.user.id
        try {
         await project.save() 
         res.send('Proyecto creado correctamente')
--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,12 +1,14 @@
 import mongoose,{ Schema, Document, PopulatedDoc, Types} from "mongoose";
 import { ITask } from "./Task";
+import { IAuth } from "./Auth";
 
 
 export interface IProject extends Document {
     projectName:string
     clientName:string
     description:string
-    tasks:PopulatedDoc<ITask & Document>[]
+    tasks:PopulatedDoc<ITask & Document>[],
+    manager:PopulatedDoc<IAuth & Document>
 }
 
 const ProjectSchema: Schema= new Schema({
@@ -30,7 +32,11 @@ const ProjectSchema: Schema= new Schema({
             type:Types.ObjectId,
             ref:'Task'
         }
-    ]
+    ],
+    manager:{
+        type:Types.ObjectId,
+        ref:'Auth'
+    }
 },{timestamps:true})
 
 const Project=mongoose.model<IProject >('Project', ProjectSchema)
